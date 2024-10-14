776894c4425c2630b01eb91ce68e677904a167d2
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-10-14 15:45:14 -0300

    Add oak framework

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
-import { handler } from "./src/server.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 const serverConfig = {
   port: 3000,
 }
 
-Deno.serve(serverConfig, handler)
+const app = new Application();
+const router = new Router();
+
+router.get("/", (ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen(serverConfig)
