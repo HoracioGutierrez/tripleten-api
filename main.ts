4aa40e26867d81d0580c306379737e7746b3d821
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-10-16 12:43:21 -0300

    Update oak module to version 17.1.0 in main.ts for improved functionality and performance

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v17.1.0/mod.ts";
 import userRouter from "./routes/userRoutes.ts";
 import cardRouter from "./routes/cardRoutes.ts";
 import { notFoundRoute } from "./routes/notFoundRoute.ts";
